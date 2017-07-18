06af2705ed5eaeb24bade77d10dd89ee2e67d160
Ashwani.Kumar <Ashwanikumarindian@gmail.com>
2017-07-18 18:13:26 +0530

    initial commits

--- src/main/java/ashwani/practise/dp/Factorial.java
+++ src/main/java/ashwani/practise/dp/Factorial.java
@@ -7,35 +7,26 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * <div class="problem-content regular problem-description">
  * <p>
- * Given an integer NN, print the factorial of the <b>N (mod 10^9+7)</b>.
+ * Given an integer N, print the factorial of the <b>N (mod 10^9+7)</b>.
  * </p>
- * <p>
  * <strong>Input:</strong><br>
  * First line contains one integer,T, number of test cases. Each test case
  * contains one integer, N
- * <p>
  * <strong>Output:</strong><br>
  * For each test case you need to print the factorial of <b>N (mod 10^9+7)</b>.
  * </p>
- * <p>
  * <strong>Constraints:</strong><br>
- * <p>
- * 1 ≤ T ≤ 10^5
- * </p>
- * <p>
- * 0 ≤ N ≤ 10^5
- * </p>
- * </div>
+ * 1 ≤ T ≤ 10^5 
+ * <br> 0 ≤ N ≤ 10^5 
  * 
  * @author Ashwani.Kumar
+ * @createdOn 18-July-2017
  *
  */
 public class Factorial {
 	static final int M = 1000000007;
 	static final long[] FACT_RESULT = new long[100000]; 
-	//facti = facti-1 * i
 	public static void main(String[] args) {
 		Arrays.fill(FACT_RESULT, -1);
 		try (Scanner sc = new Scanner(System.in);) {
