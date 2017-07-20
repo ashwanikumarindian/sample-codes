5ee42c31bb623985f3ddfb40feeb750b014a0bd8
Ashwani.Kumar <Ashwanikumarindian@gmail.com>
2017-07-20 13:23:25 +0530

    using DP and memorization in factorial

--- src/main/java/ashwani/practise/dp/Factorial.java
+++ src/main/java/ashwani/practise/dp/Factorial.java
@@ -27,25 +27,33 @@ import java.util.Scanner;
 public class Factorial {
 	static final int M = 1000000007;
 	static final long[] FACT_RESULT = new long[100000]; 
+	static int maxCachedFact = 2;
 	public static void main(String[] args) {
 		Arrays.fill(FACT_RESULT, -1);
+		FACT_RESULT[0] = 1;
+		FACT_RESULT[1] = 1;
+		FACT_RESULT[2] = 2;
 		try (Scanner sc = new Scanner(System.in);) {
 			int T = sc.nextInt();
 			int[] nums = new int[T];
 			for (int i = 0; i < T; i++) {
 				nums[i] = sc.nextInt();
 			}
+			StringBuilder result = new StringBuilder();
 			for (int i = 0; i < T; i++) {
-				System.out.println(computeFact(nums[i]));
+				result.append(computeFact(nums[i])).append(System.lineSeparator());
 			}
+			System.out.println(result.toString());
 		}
 	}
 	private static long computeFact(int i) {
-		if(FACT_RESULT[i] != -1){
+		if(i<= maxCachedFact)
 			return FACT_RESULT[i];
+		for(int j=maxCachedFact+1;j<=i;j++){
+			FACT_RESULT[j]= ((j%M) * (FACT_RESULT[j-1]%M))%M;
 		}
-		if(i <= 1)return 1;
-		return FACT_RESULT[i]=((i%M) * (computeFact(i-1)%M))%M;
+		maxCachedFact = i;
+		return FACT_RESULT[i];
 	}
 	
 }
